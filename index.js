617b2ad6705a37efac080ab6fc2b82580ce95b38
Casparas <termissues@gmail.com>
2022-08-11 02:42:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+let rotation = 1;
+async function fetchText() {
+    const response = await fetch(`https://api.adviceslip.com/advice/${Math.floor(Math.random()*220)}`);
+
+    if (response.status === 200) {
+        let data = await response.json();
+        let adviceID = data.slip.id;
+        let adviceText = data.slip.advice;
+
+        document.querySelector('.advice-number').innerHTML = `Advice#<span id="advice-id">${adviceID}</span>`;
+        document.querySelector('.advice-text').innerHTML = `“${adviceText}”`;
+    }
+}
+
+fetchText();
+
+document.querySelector("#refresh-button").addEventListener("click", function (event) {
+    document.images[1].style.transform = `rotate(${360*rotation}deg)`;
+    rotation++;
+    fetchText();
+});
+
+// Reikia padaryt ir gaut data is API greiciau, nes dabar yra await timeout.
+
+// fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data => console.log(data));
+
+
+
+
+
